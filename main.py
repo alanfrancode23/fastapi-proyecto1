c23148822e138cc4c674ad8dc00571e1684e7711
Alan Franco <alankssocial@gmail.com>
2025-06-07 02:40:21 -0400

    Guardando avance

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+posts =  []
+
+@app.get("/")
+def read_root():
+    return {"Hola": "Bienvenido a mi Proyecto"}
+
+@app.get("/posts")
+def get_posts():
+    return posts
